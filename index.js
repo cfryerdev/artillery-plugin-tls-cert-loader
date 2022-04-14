ebd238fdf1d4a35f71a01a7ede5f38441faebb99
Chris Fryer <chris.fryer@carlotz.com>
2022-04-14 14:25:06 -0600

    attempting debug

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const fs = require('fs');
 module.exports.Plugin = TlsCertLoaderPlugin;
 
 function TlsCertLoaderPlugin ({ tls }, events) {
+  console.log('this.config', this.config);
   const self = this;
   if (!self.config.tls) {
     throw new Error('Plugin: TLS config node not found. Aborting.');
   }
+  
   for (var attr in self.config.tls) {
       try {
         this.config.tls[attr] = fs.readFileSync(tls[attr])
