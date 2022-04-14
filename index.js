bebf66437c29636ded5086fd5f6b13ffec7bf9ec
Chris Fryer <chris.fryer@carlotz.com>
2022-04-14 14:50:53 -0600

    working through items

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const fs = require('fs');
 module.exports.Plugin = TlsCertLoaderPlugin;
 
 function TlsCertLoaderPlugin (script, events) {
-  console.log('script', script);
   const self = this;
-  console.log('self', self);
+  self.config = script.config.plugins['tls-cert-loader'];
+
   if (!self.config.tls) {
     throw new Error('Plugin: TLS config node not found. Aborting.');
   }
   
+  console.log('tls', self.config.tls)
   for (var attr in self.config.tls) {
       try {
-        this.config.tls[attr] = fs.readFileSync(tls[attr])
+        script.config.tls[attr] = fs.readFileSync(self.config.tls[attr])
       } catch (e) {
           console.error(`Unable to load [${attr}] from [${tls[attr]}]`);
       }
